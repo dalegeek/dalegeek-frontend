1c45e6775b91a97ec376c3dc0e24923d0d4a6b10
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2021-04-19 08:02:17 -0300

    attempting build chain

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,9 @@ npm version patch -m 'deploying %s to production'
 git push -u origin main
 git branch -D production
 git checkout -b production
+sed -i "s/\/build/#\/build/gi" .gitignore
+npm run build
+git add /build
+git commit -m 'build'
 git push -f -u origin production
 git checkout main
