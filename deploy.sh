ed80f2ed0d23609a4e9f142434a0937d0488da83
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2021-04-19 07:55:21 -0300

    adding first version of deploy file

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git checkout main
+npm version patch -m 'deploying %s to production'
+git push -u origin main
+git branch -D production
+git checkout -b production
+git push -f -u origin production
